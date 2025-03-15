0b9b2d490dd866bcdd94f71afce2d65f3e42c56c
rafael vincent <smurf4812@gmail.com>
2025-03-15 14:27:58 +0800

    changes for the sake of github

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('night-mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('night-mode');
     document.querySelector('.primary-header').classList.toggle('night-mode');
+    document.getElementById("horizontal").classList.toggle('night-mode')
 });
